394e4f7f6ce835f2a557d0edcb8689890b32be52
nyagz <44738554+nyagz@users.noreply.github.com>
2021-01-23 23:22:39 +0000

    Get langitude and longitude using expo-location

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Location from 'expo-location';
+
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    load()
+  , []})
+
+  async function load() {
+    try{
+      let { status } = await Location.requestPermissionsAsync()
+
+      if(status !== 'granted'){
+        setErrorMessage('Access to location is needed to run the app')
+        alert('Location has been allowed')
+        return 
+      } else {
+        alert('Location has been allowed')
+      }
+
+      const location = await Location.getCurrentPositionAsync()
+      const { longitude, latitude } = location.coords
+      alert(`Latitude: ${latitude}, longitude: ${longitude}`)
+      
+    } catch{
+
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>Hello from Teta</Text>
